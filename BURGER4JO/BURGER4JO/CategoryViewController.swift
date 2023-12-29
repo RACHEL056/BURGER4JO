d7f1697d19f6b414fc7315b58c463668b19e9502
hay-yo <91271462+hay-yo@users.noreply.github.com>
2023-12-29 10:21:03 +0900

    last commit
    1. 버튼 구현 방식을 코드베이스로 바꿈(스토리보드 사용 안하는 방식)
    2. 임시 vc 삭제 완료

--- BURGER4JO/BURGER4JO/CategoryViewController.swift
+++ BURGER4JO/BURGER4JO/CategoryViewController.swift
@@ -7,18 +7,27 @@
 
 import UIKit
 
+// 오리엔테이션 설정 - 가로 모드 안되도록 설정
+
 class CategoryViewController: UIViewController {
     // MARK: - Properties
-    @IBOutlet weak var burgerButton: UIButton!
-    @IBOutlet weak var sideButton: UIButton!
-    @IBOutlet weak var drinkButton: UIButton!
+    let burgerButton = UIButton()
+    let sideButton = UIButton()
+    let drinkButton = UIButton()
     let imageView = UIImageView()
     
+    // MARK: - viewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // MARK: - Background Color
         view.backgroundColor = UIColor(#colorLiteral(red: 1, green: 0.6729137301, blue: 0.3843368888, alpha: 1))
+        
+        // MARK: - Button Setup
+        // 순서가 중요 - constraints를 설정하기 전에 먼저 버튼을 설정해야함
+        setupButton(burgerButton, title: "버거")
+        setupButton(sideButton, title: "사이드")
+        setupButton(drinkButton, title: "음료")
         
         // MARK: - Image
         imageView.translatesAutoresizingMaskIntoConstraints = false
@@ -27,22 +36,22 @@ class CategoryViewController: UIViewController {
         }
         view.addSubview(imageView)
         imageView.contentMode = .scaleAspectFit
-       
-        NSLayoutConstraint.activate([
-            imageView.topAnchor.constraint(equalTo: burgerButton.bottomAnchor, constant: 23),
-            imageView.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor, constant: 0),
-            imageView.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor, constant: 0),
-            imageView.bottomAnchor.constraint(equalTo: view.layoutMarginsGuide.bottomAnchor, constant: -450)
-            //나중에 병합 후에는 bottomAnchor의 constraint를 바꿔야할 수도?
-        ])
         
-        // MARK: - Button Setup
-        setupButton(burgerButton, title: "버거")
-        setupButton(sideButton, title: "사이드")
-        setupButton(drinkButton, title: "음료")
+        // imageView를 superView가 리사이징 될 때 같이 리사이징 되도록 설정
+        imageView.setContentCompressionResistancePriority(.defaultLow, for: .horizontal)
+        imageView.setContentCompressionResistancePriority(.defaultLow, for: .vertical)
        
         // MARK: - AutoLayout
-        // 버거 constraints
+        // 이미지 constraints
+        NSLayoutConstraint.activate([
+            imageView.topAnchor.constraint(equalTo: burgerButton.bottomAnchor, constant: 10),
+            imageView.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor, constant: 0),
+            imageView.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor, constant: 0),
+            imageView.bottomAnchor.constraint(equalTo: view.layoutMarginsGuide.bottomAnchor, constant: -300)
+            //나중에 병합 후에는 이미지뷰의 bottomAnchor의 constraint를 바꿔야할 수도?
+        ])
+        
+        // 버거 버튼 constraints
         NSLayoutConstraint.activate([
             burgerButton.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor, constant: 10),
             burgerButton.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor),
@@ -50,7 +59,7 @@ class CategoryViewController: UIViewController {
             burgerButton.widthAnchor.constraint(equalTo: sideButton.widthAnchor)
         ])
         
-        // 사이드 constraints
+        // 사이드 버튼 constraints
         NSLayoutConstraint.activate([
             sideButton.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor, constant: 10),
             sideButton.leadingAnchor.constraint(equalTo: burgerButton.trailingAnchor),
@@ -58,7 +67,7 @@ class CategoryViewController: UIViewController {
             sideButton.widthAnchor.constraint(equalTo: drinkButton.widthAnchor)
         ])
         
-        // 음료 constraints
+        // 음료 버튼 constraints
         NSLayoutConstraint.activate([
             drinkButton.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor, constant: 10),
             drinkButton.leadingAnchor.constraint(equalTo: sideButton.trailingAnchor),
@@ -80,8 +89,8 @@ class CategoryViewController: UIViewController {
         button.setTitleColor(.white, for: .normal)
         button.layer.cornerRadius = 10
         button.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(button)
     }
-    
     
     // MARK: - Button Helper
     // 세부 메뉴를 보여줌 - BurgerViewController() 창이 팝업된다
@@ -89,13 +98,11 @@ class CategoryViewController: UIViewController {
         let burgerVc = BurgerViewController()
         self.present(burgerVc, animated: true, completion: nil)
     }
-    
     // SideViewController()
     @objc func didTapSideButton() {
         let sideVc = SideViewController()
         self.present(sideVc, animated: true, completion: nil)
     }
-    
     // drinkViewController()
     @objc func didTapDrinkButton() {
         let drinkVc = DrinkViewController()
